2efe836bb58ebb6ae4d67dfc7d4276c8adad7daa
Murrax2 <murrax2@gmail.com>
2019-01-28 15:12:08 +0000

    Update run.py for compatibility
    Some machines have python installed as py while some as python. Bot now works regardless

--- run.py
+++ run.py
@@ -2,10 +2,16 @@ import os
 import threading
 
 def bootBot():
-	os.system('py bootbot.py')
+	try:
+		os.system('py bootbot.py')
+	except:
+		os.system('python bootbot.py')
 
 def loopBot():
-	os.system('py loopbotstart.py')
+	try:
+		os.system('py loopbotstart.py')
+	except:
+		os.system('python loopbotstart.py')
 
 botThread = threading.Thread(name='botThread', target=bootBot)
 loopThread = threading.Thread(name='loopThread', target=loopBot)

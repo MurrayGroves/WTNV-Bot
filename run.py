9fd22bc58bedeee97d200d5001f5134b8efa5040
Murrax2 <murrax2@gmail.com>
2019-09-21 17:40:53 +0100

    Fix issue starting bot in linux

--- run.py
+++ run.py
@@ -5,13 +5,13 @@ def bootBot():
 	if os.name == 'nt':
 		os.system('py bootbot.py')
 	else:
-		os.system('python bootbot.py')
+		os.system('python3 bootbot.py')
 
 def loopBot():
 	if os.name == 'nt':
 		os.system('py loopbotstart.py')
 	else:
-		os.system('python loopbotstart.py')
+		os.system('python3 loopbotstart.py')
 
 botThread = threading.Thread(name='botThread', target=bootBot)
 loopThread = threading.Thread(name='loopThread', target=loopBot)
